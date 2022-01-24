3f0b94c5ba2b387be29db96c6b08fae803f2499d
Francesco Bianco <info.francescobianco@gmail.com>
2022-01-24 15:25:27 +0100

    Update webrequest.php

--- webrequest.php
+++ webrequest.php
@@ -1,1 +1,9 @@
+<?php
 
+$image = imagecreatetruecolor(1, 1);
+$color = imagecolorallocate($image, 255, 255, 255); 
+
+imagesetpixel($image, 0, 0, $color);  
+
+header('Content-Type: image/png');
+imagepng($image);
